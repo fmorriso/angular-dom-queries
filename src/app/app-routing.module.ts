115fa94e7b636c94bb457ee981f9aaac5d3d701c
Fred Morrison <fpmorrison@verizon.net>
2020-02-23 09:02:17 -0700

    upgrade to Angular 9.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import {Routes, RouterModule, ExtraOptions} from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 //
-import {HomeComponent} from './home/home.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {HelloComponent} from './hello/hello.component';
-
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HelloComponent } from './hello/hello.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, data: { label: 'Home' } },
@@ -21,4 +20,4 @@ const routeOptions: ExtraOptions = {
   imports: [RouterModule.forRoot(routes, routeOptions)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
